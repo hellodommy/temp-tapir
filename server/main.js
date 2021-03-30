1fd4f5e440c7e80a08c0d4f2c13b027caf1ea0b6
hellodommy <dominique@u.nus.edu>
2021-03-30 16:19:21 +0800

    Convert CSV to JSON with organisation

--- server/main.js
+++ server/main.js
@@ -1,9 +1,40 @@
 import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
+import Papa from "papaparse";
 
 function insertLink({ title, url }) {
   LinksCollection.insert({title, url, createdAt: new Date()});
 }
+
+Meteor.startup(() => {
+  const filename = "room-temperatures.csv";
+  const csvText = Assets.getText(filename);
+  const results = Papa.parse(csvText, { delimiter: ",", header: true }).data;
+  let data = {};
+  for (const [key, value] of Object.entries(results)) {
+    const timestamp = value.timestamp;
+    const [date, time] = [timestamp.split("T")[0], timestamp.split("T")[1]];
+    const roomId = value.RoomId;
+    const temp = value.temperature;
+    if (!(data.hasOwnProperty(date))) {
+      // date not in
+      data[date] = {}
+      data[date][time] = {};
+      data[date][time][roomId] = temp;
+    } else {
+      // date already inside
+      if (!data[date].hasOwnProperty(time)) {
+        // time not in
+        data[date][time] = {}
+        data[date][time][roomId] = temp;
+      } else {
+        data[date][time][roomId] = temp;
+      }
+    }
+  }
+  console.log(JSON.stringify(data));
+});
+
 
 Meteor.startup(() => {
   // If the Links collection is empty, add some data.
