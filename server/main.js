4f84048baf8055849344293f1246ad2183a24ed4
hellodommy <dominique@u.nus.edu>
2021-03-30 16:45:28 +0800

    Move JSON organisation to its own function

--- server/main.js
+++ server/main.js
@@ -6,33 +6,37 @@ function insertLink({ title, url }) {
   LinksCollection.insert({title, url, createdAt: new Date()});
 }
 
+function organiseData(results) {
+ let data = {};
+ for (const [key, value] of Object.entries(results)) {
+   const timestamp = value.timestamp;
+   const [date, time] = [timestamp.split("T")[0], timestamp.split("T")[1]];
+   const roomId = value.RoomId;
+   const temp = value.temperature;
+   if (!data.hasOwnProperty(date)) {
+     // date not in
+     data[date] = {};
+     data[date][time] = {};
+     data[date][time][roomId] = temp;
+   } else {
+     // date already inside
+     if (!data[date].hasOwnProperty(time)) {
+       // time not in
+       data[date][time] = {};
+       data[date][time][roomId] = temp;
+     } else {
+       data[date][time][roomId] = temp;
+     }
+   }
+ }
+ return data;
+}
+
 Meteor.startup(() => {
   const filename = "room-temperatures.csv";
   const csvText = Assets.getText(filename);
   const results = Papa.parse(csvText, { delimiter: ",", header: true }).data;
-  let data = {};
-  for (const [key, value] of Object.entries(results)) {
-    const timestamp = value.timestamp;
-    const [date, time] = [timestamp.split("T")[0], timestamp.split("T")[1]];
-    const roomId = value.RoomId;
-    const temp = value.temperature;
-    if (!(data.hasOwnProperty(date))) {
-      // date not in
-      data[date] = {}
-      data[date][time] = {};
-      data[date][time][roomId] = temp;
-    } else {
-      // date already inside
-      if (!data[date].hasOwnProperty(time)) {
-        // time not in
-        data[date][time] = {}
-        data[date][time][roomId] = temp;
-      } else {
-        data[date][time][roomId] = temp;
-      }
-    }
-  }
-  console.log(JSON.stringify(data));
+  const data = organiseData(results);
 });
 
 
