74b7d8808e1cd2fa44b93539edd75df43b38040d
hellodommy <dominique@u.nus.edu>
2021-03-30 19:10:18 +0800

    Add data to collections (not tested)

--- server/main.js
+++ server/main.js
@@ -1,66 +1,76 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { TempCollection } from "../imports/db/TempCollection";
 import Papa from "papaparse";
-
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
-
-function organiseData(results) {
- let data = {};
- for (const [key, value] of Object.entries(results)) {
-   const timestamp = value.timestamp;
-   const [date, time] = [timestamp.split("T")[0], timestamp.split("T")[1]];
-   const roomId = value.RoomId;
-   const temp = value.temperature;
-   if (!data.hasOwnProperty(date)) {
-     // date not in
-     data[date] = {};
-     data[date][time] = {};
-     data[date][time][roomId] = temp;
-   } else {
-     // date already inside
-     if (!data[date].hasOwnProperty(time)) {
-       // time not in
-       data[date][time] = {};
-       data[date][time][roomId] = temp;
-     } else {
-       data[date][time][roomId] = temp;
-     }
-   }
- }
- return data;
-}
 
 Meteor.startup(() => {
   const filename = "room-temperatures.csv";
   const csvText = Assets.getText(filename);
   const results = Papa.parse(csvText, { delimiter: ",", header: true }).data;
-  const data = organiseData(results);
-});
-
-
-Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
+  
+  for (const [key, value] of Object.entries(results)) {
+    const timestamp = value.timestamp;
+    const [currDate, currTime] = [
+      timestamp.split("T")[0],
+      timestamp.split("T")[1],
+    ];
+    const [currHour, currMin] = [
+      currTime.split(":")[0],
+      currTime.split(":")[1],
+    ];
+    const currRoom = value.RoomId;
+    const currTemp = value.temperature;
+    if (
+      TempCollection.find({
+        room: currRoom,
+        date: currDate,
+        "data.hour": currHour,
+      }).count() != 0
+    ) {
+      // hour is found
+      TempCollection.update(
+        {
+          room: currRoom,
+          date: currDate,
+          "data.hour": currHour,
+        },
+        {
+          $addToSet: {
+            "data.$.details": {
+              min: currMin,
+              temp: currTemp,
+            },
+          },
+        }
+      );
+    } else if (
+      TempCollection.find({ room: currRoom, date: currDate }).count() != 0
+    ) {
+      // date is found
+      TempCollection.update(
+        {
+          room: currRoom,
+          date: currDate,
+        },
+        {
+          $addToSet: {
+            data: {
+              hour: currHour,
+              details: [
+                {
+                  min: currMin,
+                  temp: currTemp,
+                },
+              ],
+            },
+          },
+        }
+      );
+    } else {
+      TempCollection.insert({
+        room: currRoom,
+        date: currDate,
+        data: [{ hour: currHour, details: [{ min: currMin, temp: currTemp }] }],
+      });
+    }
   }
 });
