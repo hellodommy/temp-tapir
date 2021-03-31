a641eab4b9cd24893f2578e19319eca54068bab4
hellodommy <dominique@u.nus.edu>
2021-03-31 19:35:39 +0800

    Save control states in App component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,100 @@
-import React from "react";
-import { Control } from "./Control";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Slider from "@material-ui/core/Slider";
 
-export const App = () => (
-  <div>
-    <Typography variant="h3" gutterBottom>
-      Temperature Tapir 🦡
-    </Typography>
-    <Control />
-  </div>
-);
+export const App = () => {
+  const [startDate, setStartDate] = useState("2013-10-02");
+  const [startTime, setStartTime] = useState("05:00");
+  const [endDate, setEndDate] = useState("2013-12-03");
+  const [endTime, setEndTime] = useState("15:15");
+  const [sampleSize, setSampleSize] = useState(25);
+
+  return (
+    <div>
+      <Typography variant="h3" gutterBottom>
+        Temperature Tapir 🦡
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={3}>
+          <form noValidate>
+            <TextField
+              id="startDate"
+              label="Start Date"
+              type="date"
+              defaultValue={startDate}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </form>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <form noValidate>
+            <TextField
+              id="startTime"
+              label="Start Time"
+              type="time"
+              defaultValue={startTime}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              inputProps={{
+                step: 300, // 5 min
+              }}
+              onChange={(e) => setStartTime(e.target.value)}
+            />
+          </form>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <form noValidate>
+            <TextField
+              id="endDate"
+              label="End Date"
+              type="date"
+              defaultValue={endDate}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </form>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <form noValidate>
+            <TextField
+              id="endTime"
+              label="End Time"
+              type="time"
+              defaultValue={endTime}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              inputProps={{
+                step: 300, // 5 min
+              }}
+              onChange={(e) => setEndTime(e.target.value)}
+            />
+          </form>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="body2" gutterBottom>
+            Sample size
+          </Typography>
+          <Slider
+            defaultValue={sampleSize}
+            aria-labelledby="discrete-slider-small-steps"
+            step={1}
+            marks
+            min={1}
+            max={50}
+            valueLabelDisplay="auto"
+            onChange={(e, v) => setSampleSize(v)}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}; 
